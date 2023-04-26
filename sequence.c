017012b4b470d8846a12da600de5ac7be4097286
a1849716 <109998412+a1849716@users.noreply.github.com>
2023-04-26 14:46:06 +0930

    Update sequence.c

--- sequence.c
+++ sequence.c
@@ -1,39 +1,21 @@
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
 int main() {
-  // open and reading file
-  FILE *fp;
-  fp = fopen("cmdfile", "r");
-  if (fp == NULL) {
-    fprintf(stderr, "failed to open file\n");
-    return 1;
-  }
-  printf("file opened for reading\n");
+  char curr_line[255];
 
-  bool keep_reading = true;
-
-  //forking
+  // forking
   int pid = fork();
   int run = 1;
-  
-  //while there are still lines
-  while (keep_reading) {
-    // string to store commands
-    char *commands[10];
-    // stop reading
-    if (feof(fp)) {
-      keep_reading = false;
-    }
 
-    //strtok the current line into command[0]
-    char curr_line[255];
-    fgets(curr_line, 255, fp);
+  // while there are still lines
+  while (fgets(curr_line, 255, stdin) != NULL) {
+  // string to store commands
+  char *commands[12];
+    // strtok curr_line into commands[12]
     char *token = strtok(curr_line, " ");
     int i = 0;
     while (token != NULL) {
@@ -49,14 +31,12 @@ int main() {
       return 1;
     } else if (pid == 0) {  // child
       run = execvp(commands[0], commands);
-      printf("run %s: %d\n",curr_line, run);
-    } else if (pid > 1) {   //parent
+      fprintf("run %s: %d\n", curr_line, run);
+    } else if (pid > 1) {  // parent
       printf("Start\n");
       wait(NULL);
     }
   }
-  // use strtok
-  fclose(fp);
   printf("\n");
 
   return 0;
